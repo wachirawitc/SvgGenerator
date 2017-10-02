8453746fa9ffbaa60058a18c1936461b1802e5df
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2017-10-02 21:34:36 +0700

    Update map

---
+++ SvgGenerator/Controllers/TemplateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SvgGenerator.Controllers
+{
+    public class TemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public OkResult SaveWall(int templateId, int row, int column)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public OkResult RemoveWall(int templateId, int row, int column)
+        {
+            return Ok();
+        }
+    }
+}
