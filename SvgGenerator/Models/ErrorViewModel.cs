cff0629799c6d08c8c7e8650964efa53479a5aaa
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2017-10-02 18:27:50 +0700

    Create project

---
+++ SvgGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SvgGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
